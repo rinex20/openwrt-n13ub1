b79b60cacb33ccf8176fcc6b6cc91720a7d11582
rinex20 <rinex20@me.com>
2016-10-19 12:42:37 +0800

    Update install.sh

--- miwifi_mini/install.sh
+++ miwifi_mini/install.sh
@@ -13,3 +13,9 @@ opkg install iptables-mod-nat-extra ipset libopenssl
 #Extra package 2
 opkg install kmod-nfnetlink kmod-ipt-ipset kmod-ipt-tproxy libmnl libpthread
 
+#download shadowsocks-libev
+cd /tmp
+wget --no-check-certificate https://raw.githubusercontent.com/rinex20/openwrt-n13ub1/master/tmp/shadowsocks-libev_2.5.5-1_ramips_24kec.ipk
+
+wget --no-check-certificate https://raw.githubusercontent.com/rinex20/openwrt-n13ub1/master/tmp/
+
